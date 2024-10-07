4e4320442719442aaac31aa49716f1002c65a4d5
Hanriiel <hannariikka.happonen@gmail.com>
2024-10-07 09:06:24 +0300

    Lisätty etusivu Thymeleafilla, ei vielä toimi muuten

--- src/main/java/vegit/web/vegitController.java
+++ src/main/java/vegit/web/vegitController.java
@@ -2,15 +2,13 @@ package vegit.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class vegitController {
 
-    @RequestMapping("main")
-    @ResponseBody
-    public String naytaViesti(){
-        return "Toimii";
+    @RequestMapping("/")
+    public String index(){
+        return "index";
     }
 
 
